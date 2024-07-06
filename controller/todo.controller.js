86b6247259915e02baa60dd024aca860524017af
Komal Jadhav <komalmjadhav25@gmail.com>
2024-07-06 12:47:55 +0530

    ui added

--- controller/todo.controller.js
+++ controller/todo.controller.js
@@ -1,18 +1,18 @@
-const Todo = require("../module/Todo")
+const Todos = require("../module/Todos")
 
 exports.getAllTodos = async (req, res) => {
-    const result = await Todo.find()
+    const result = await Todos.find()
     res.json({ message: "Todo fetch success", result })
 }
 exports.addTodos = async (req, res) => {
-    await Todo.create(req.body)
+    await Todos.create(req.body)
     res.json({ message: "Todo craete success" })
 }
 exports.updateTodos = async (req, res) => {
-    await Todo.findByIdAndUpdate(req.params.id, req.body)
+    await Todos.findByIdAndUpdate(req.params.id, req.body)
     res.json({ message: "Todo update success" })
 }
 exports.deleteTodos = async (req, res) => {
-    await Todo.findByIdAndDelete(req.params.id)
+    await Todos.findByIdAndDelete(req.params.id)
     res.json({ message: "Todo delete success" })
 }
